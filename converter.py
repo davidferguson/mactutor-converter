aa08ac4464019c2ec11297a4ba4d7f01d693ade0
David Ferguson <davidferguson@users.noreply.github.com>
2019-08-07 13:27:29 +0100

    chronology and project conversion

--- converter.py
+++ converter.py
@@ -16,7 +16,6 @@ import lektor.metaformat
 import biographyparser
 import extrasparser
 import historytopicsparser
-import datasheetparser
 import honoursparser
 import societiesparser
 import quotationsparser
@@ -24,6 +23,12 @@ import obituariesparser
 import curvesparser
 import emsparser
 import glossaryparser
+
+import datasheetparser
+import htmlparser
+import flow
+import cleaning
+import referenceparser
 
 LEKTOR_CONTENT_PATH = '/Users/david/Documents/MacTutor/actual-work/lektor-davidferguson/mactutor/content/'
 
@@ -73,6 +78,116 @@ def convert(input_dir, output_dir, skip_fn, converter, url_context):
         print('processed', datasheet['FILENAME'])
 
 
+def chronology_convert(input_dir, output_dir, url_context):
+    # get all the files that need to be processed
+    path = os.path.join(input_dir, '*')
+    files = glob.glob(path)
+
+    dates = {}
+
+    # process all the files
+    for file in files:
+        # parse sections from datasheet
+        datasheet = datasheetparser.parse_file(file)
+        date = datasheet['DATE']
+
+        content = htmlparser.parse(datasheet['BIG'], os.path.basename(file), paragraphs=False, url_context=url_context)
+        data = {
+            'about': 'yes' if datasheet['ABOUT'] != '' else 'no',
+            'content': content
+        }
+
+        if date not in dates:
+            dates[date] = []
+        dates[date].append(data)
+
+    # convert to nested flow
+    chronology = []
+    for date, events in dates.items():
+        data = {
+            'date': date,
+            'events': flow.to_flow_block('chronology-event', events)
+        }
+        chronology.append(data)
+    chronology = flow.to_flow_block('chronology', chronology)
+
+    data = {
+        '_model': 'chronology',
+        '_template': 'chronology.html',
+        'title': 'Chronology',
+        'chronology': chronology
+    }
+    filename = os.path.join(LEKTOR_CONTENT_PATH, output_dir)
+    save(data, filename)
+
+
+def project_convert(input_dir, output_dir, url_context, name):
+    # get all the files that need to be processed
+    path = os.path.join(input_dir, '*')
+    files = glob.glob(path)
+
+    titles = {
+        'Ayel': 'The French Grandes Ecoles',
+        'Brunk': 'The development of Galois theory',
+        'Burslem': 'Sofia Kovalevskaya',
+        'Daxenberger': 'Johan de Witt - The first calculation on the valuation of life annuities',
+        'Ellison': 'Sofia Kovalevskaya',
+        'Johnson': 'James Clerk Maxwell - The Great Unknown',
+        'MacQuarrie': 'Mathematics and Chess',
+        'Pearce': 'Indian Mathematics - Redressing the balance',
+        'Watson': 'Some topics in the history of mathematical education',
+        'Ledermann': 'Walter Ledermann - Encounters of a Mathematician'
+    }
+    authors = {
+        'Ayel': 'Mathieu Ayel',
+        'Brunk': 'Fiona Brunk',
+        'Burslem': 'Tom Burslem',
+        'Daxenberger': 'Livia Daxenberger',
+        'Ellison': 'Leigh Ellison',
+        'Johnson': 'Kevin Johnson',
+        'MacQuarrie': 'John MacQuarrie',
+        'Pearce': 'Ian G Pearce',
+        'Watson': 'Helen Watson',
+        'Ledermann': 'JOC/EFR'
+    }
+
+    pages = []
+    references = None
+
+    # process all the files
+    for file in files:
+        # parse sections from datasheet
+        datasheet = datasheetparser.parse_file(file)
+        if datasheet['NUMBER'] == 'refs' and 'REFERENCES' in datasheet:
+            # this is the references, not a page
+            references = referenceparser.parse_references(datasheet['REFERENCES'], file, url_context)
+            references = flow.to_flow_block('reference', json.loads(references)['data'])
+            continue
+
+        pagenum = int(datasheet['NUMBER'])
+        assert pagenum == len(pages)
+
+        content = cleaning.project_cleaning(datasheet['CONTENT'])
+        data = {
+            'title': datasheet['TITLE'],
+            'content': htmlparser.parse(content, file, paragraphs=True, url_context=url_context)
+        }
+        pages.append(data)
+
+    # convert to flow
+    data = {
+        '_model': 'project',
+        '_template': 'project.html',
+        'title': titles[name],
+        'author': authors[name],
+        'pages': flow.to_flow_block('projectpage', pages),
+        'references': '' if references is None else references
+    }
+    filename = os.path.join(LEKTOR_CONTENT_PATH, output_dir)
+    save(data, filename)
+    print('processed', name)
+
+
 if __name__ == '__main__':
     skip = lambda datasheet: '<table' in datasheet['BIOGRAPHY']
     convert('../datasheets/Biographies', 'Biographies', skip, biographyparser, 'Biographies/')
@@ -115,3 +230,12 @@ if __name__ == '__main__':
     files_to_process = ['Strick/', 'Tait/', 'Wallace/', 'Wallace/butterfly', 'Curves/Definitions', 'Curves/Definitions2']
     skip = lambda datasheet: datasheet['FILENAME'] not in files_to_process and not datasheet['FILENAME'].startswith('Astronomy')
     convert('../datasheets/Files', '', skip, emsparser, '/')
+
+    chronology_convert('../datasheets/Chronology', 'Chronology', 'Chronology/')
+
+    # special case for Ledermann
+    project_convert('../datasheets/Projects/Ledermann/', 'Ledermann', 'Ledermann/', 'Ledermann')
+
+    projects = ['Ayel','Brunk','Burslem','Daxenberger','Ellison','Johnson','MacQuarrie','Pearce']#,'Watson']
+    for project in projects:
+        project_convert('../datasheets/Projects/%s/' % project, 'Projects/%s' % project, 'Projects/%s/' % project, project)
