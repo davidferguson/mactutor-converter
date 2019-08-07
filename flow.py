f1132d23c5bc31e963ae2e8dd12538a4c3b35356
David Ferguson <davidferguson@users.noreply.github.com>
2019-08-07 13:30:10 +0100

    allow nested flow blocks

--- flow.py
+++ flow.py
@@ -1,4 +1,10 @@
 import lektor.metaformat
+import re
+_block_re = re.compile(r'^###(?:#+)\s*([^#]*?)\s*###(?:#+)\s*$')
+
+def _line_is_flowheader(line):
+    match = _block_re.search(line)
+    return match
 
 def to_flow_block(block_name, array):
     output = []
@@ -6,5 +12,11 @@ def to_flow_block(block_name, array):
         items = list(item.items())
         output.append('#### %s ####\n' % block_name)
         lektordata = list(lektor.metaformat.serialize(items))
+
+        for idx, line in enumerate(lektordata):
+            if _line_is_flowheader(line):
+                lektordata[idx] = u'#' + line.strip() + u'#\n'
+
         output += lektordata
+
     return ''.join(output)
